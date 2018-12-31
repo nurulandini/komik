a01465b3573fa4037c5d14bb953a42fa243a2f67
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-31 13:20:32 +0700

    popular

--- application/views/userview/popular.php
+++ application/views/userview/popular.php
@@ -1,7 +1,36 @@
 <div class="row">
 	<div class="col-md-12">
-		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 250px;">
-			<p>POPULAR</p>
+		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 1300px;">
+			<table>
+				<tr>
+					<td>
+						<img src="<?php echo base_url()?>/img/adventure1.png" style="height:150px; width:100px; margin-top: 40px; margin-left: 35px; width: 160px; height: 220px;">
+					</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td>
+						POKEMON AND RED DO A TRAINING CHALLENGE AND.....NEVER COMES BACK!!!.....WILL THEY BE FOUNDED?
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+					</td>
+				</tr>
+			</table>
 		</div>
 	</div>
 </div>
