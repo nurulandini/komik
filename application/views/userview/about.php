a71fce40243cfd78a967b182747ff7443d1c01c2
ayakwae <39100835+ayakwae@users.noreply.github.com>
2018-12-31 17:01:28 +0700

    Update about.php

--- application/views/userview/about.php
+++ application/views/userview/about.php
@@ -1,6 +1,9 @@
 <div class="row">
 	<div class="col-md-12">
-		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 250px;">
+		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 500px;">
+			<div id="demo" class="carousel slide" data-ride="carousel">
+
+			</div>
 		</div>
 	</div>
 </div>
