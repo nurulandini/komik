355f69a7012f9fac59420059d3ae9e0d27083f0f
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-31 23:25:00 +0700

    tinggal garisny dan rating manual

--- application/views/userview/popular.php
+++ application/views/userview/popular.php
@@ -1,7 +1,15 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style type="text/css">
+	.checked {
+  color: orange;
+}
+</style>
+
 <div class="row">
 	<div class="col-md-12">
-		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 1300px;">
-			<table border="0">
+		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 1325px;">
+			<table border="0" align="center">
 				<tr>
 					<td rowspan="2">
 						<img src="<?php echo base_url()?>/img/adventure1.png" style="height:150px; width:100px; margin-top: 40px; margin-left: 20px; width: 160px; height: 220px;">
@@ -15,61 +23,88 @@
 						Sinopsis :  Bulbasaur and others that follow storylines from the videogames! PokÉmon trainer Red goes off on a training challenge and...never comes back!!!...Will they be founded?
 					</td>
 				</tr>
+				<tr>
+					<td style="color: white; font-family: times new roman;">
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>&nbsp;&nbsp;4.99
+					</td>
+				</tr>
 			<table border="0">
 				<tr>
-					<td rowspan="2">
+					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 100px;">
 						ONE PIECE
 					</td>
 				</tr>
 				<tr>
-					<td>rating</td>
+					<td style="color: white; font-family: times new roman; padding-bottom: 70px;">
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star"></span>&nbsp;&nbsp;4
+					</td>
 				</tr>
 			</table>
 			<table border="0">
 				<tr>
-					<td rowspan="2">
+					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 100px;">
 						CRAYON SHINCHAN
 					</td>
 				</tr>
 				<tr>
-					<td>
-						rating
+					<td style="color: white; font-family: times new roman; padding-bottom: 70px;">
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star" ></span>&nbsp;&nbsp;3.95
 					</td>
 				</tr>
 			</table>
 			<table border="0">
 				<tr>
-					<td rowspan="2">
+					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 100px;">
 						TINTIN AND THE PICAROS
 					</td>
 				</tr>
 				<tr>
-					<td>
-						rating
+					<td style="color: white; font-family: times new roman; padding-bottom: 70px;">
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star" ></span>&nbsp;&nbsp;3
 					</td>
 				</tr>
 			</table>
 			<table border="0">
 				<tr>
-					<td rowspan="2">
+					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 100px;">
 						TERJEBAK NOSTALGIA
 					</td>
 				</tr>
 				<tr>
-					<td>
-						rating
+					<td style="color: white; font-family: times new roman; padding-bottom: 70px;">
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star" ></span>&nbsp;&nbsp;2.99
 					</td>
 				</tr>
 			</table>
