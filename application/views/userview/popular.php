e3f41dc7721dae037aaa82e05d881079e9669cd7
Nadia Siti Namira <nadiasitinamira@gmail.com>
2018-12-31 22:32:36 +0700

    tinggal rating sama garis putih lgi dicobak

--- application/views/userview/popular.php
+++ application/views/userview/popular.php
@@ -1,33 +1,75 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 1300px;">
-			<table>
+			<table border="0">
 				<tr>
-					<td>
-						<img src="<?php echo base_url()?>/img/adventure1.png" style="height:150px; width:100px; margin-top: 40px; margin-left: 35px; width: 160px; height: 220px;">
-					</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td>
-						POKEMON AND RED DO A TRAINING CHALLENGE AND.....NEVER COMES BACK!!!.....WILL THEY BE FOUNDED?
+					<td rowspan="2">
+						<img src="<?php echo base_url()?>/img/adventure1.png" style="height:150px; width:100px; margin-top: 40px; margin-left: 20px; width: 160px; height: 220px;">
+					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td style="font-family: Tarijan Pro; font-size: 35px; text-align: center; color: white; padding-bottom: -40px; padding-top: 20px;">
+						POKÉMON ADVENTURE
+					</td>
+				</tr>
+				<tr>
+					<td width="650" style="font-family: tarijan pro; font-size: 20px; color: white; text-align: justify;">
+						Sinopsis :  Bulbasaur and others that follow storylines from the videogames! PokÉmon trainer Red goes off on a training challenge and...never comes back!!!...Will they be founded?
+					</td>
+				</tr>
+			<table border="0">
+				<tr>
+					<td rowspan="2">
+						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+						ONE PIECE
+					</td>
+				</tr>
+				<tr>
+					<td>rating</td>
+				</tr>
+			</table>
+			<table border="0">
+				<tr>
+					<td rowspan="2">
+						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+						CRAYON SHINCHAN
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+						rating
+					</td>
+				</tr>
+			</table>
+			<table border="0">
+				<tr>
+					<td rowspan="2">
+						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+						TINTIN AND THE PICAROS
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+						rating
+					</td>
+				</tr>
+			</table>
+			<table border="0">
+				<tr>
+					<td rowspan="2">
+						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-bottom: 40px;">
+						TERJEBAK NOSTALGIA
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 35px; width: 160px; height: 220px;">
+						rating
 					</td>
 				</tr>
 			</table>
