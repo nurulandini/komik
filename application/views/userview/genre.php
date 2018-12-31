fda458cfbe4c585aa509ae11fdeb7318290cffd0
ayakwae <39100835+ayakwae@users.noreply.github.com>
2018-12-31 16:31:31 +0700

    Update genre.php

--- application/views/userview/genre.php
+++ application/views/userview/genre.php
@@ -30,8 +30,8 @@
 				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
 					<table>
 						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/1.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/romance2.jpg" style="width: 160px; height: 220px;"></td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/juki.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/tahi.jpg" style="width: 160px; height: 220px;"></td>
 						</tr>
 					</table>
 				</div>
@@ -40,8 +40,8 @@
 				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
 					<table>
 						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror2.jpeg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror3.jpg" style="width: 160px; height: 220px;"></td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action2.jpeg" style="width: 160px; height: 220px;"></td>
 						</tr>
 					</table>
 				</div>
@@ -68,8 +68,8 @@
 				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
 					<table>
 						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/naruto.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/2.jpg" style="width: 160px; height: 220px;"></td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy.png" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy2.jpg" style="width: 160px; height: 220px;"></td>
 						</tr>
 					</table>
 				</div>
