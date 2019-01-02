24f6cb9b19bdf6eea19f783109778e1c712876b1
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-01-03 00:37:26 +0700

    tahla

--- application/views/userview/about.php
+++ application/views/userview/about.php
@@ -2,7 +2,7 @@
 	<div class="col-md-12">
 		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 500px;">
 	
-		 <p style="font-family: Lucida Handwriting; font-size: 35px;color: white; padding-top: 20px; text-align: center; ">ABOUT US</p><hr color="white" >
+		 <p style="font-family: Lucida Handwriting; font-size: 35px;color: white; padding-top: 20px; text-align: center; ">ABOUT US</p><hr color="white">
 
 		<table>
 		<td style="font-family: Tarijan Pro; font-size: 23px;color: white; padding-bottom: 20px;">
--- application/views/userview/popular.php
+++ application/views/userview/popular.php
@@ -19,25 +19,25 @@
 					</td>
 				</tr>
 				<tr>
-					<td width="650" style="font-family: tarijan pro; font-size: 20px; color: white; text-align: justify; padding-bottom: 20px;">
+					<td width="650" style="font-family: tarijan pro; font-size: 25px; color: white; text-align: justify; padding-bottom: 20px;">
 						Sinopsis :  Bulbasaur and others that follow storylines from the videogames! PokÉmon trainer Red goes off on a training challenge and...never comes back!!!...Will they be founded?
 					</td>
 				</tr>
 				<tr>
-					<td style="color: white; font-family: times new roman;">
+					<td style="color: white; font-family: times new roman; padding-top: 10px;">
 						<span class="fa fa-star checked"></span>
 						<span class="fa fa-star checked"></span>
 						<span class="fa fa-star checked"></span>
 						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>&nbsp;&nbsp;4.99
+						<span class="fa fa-star checked"></span>&nbsp;&nbsp;&nbsp;4.99
 					</td>
 				</tr>
 			<table border="0">
 				<tr>
 					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+						<img src="<?php echo base_url()?>/img/onepiece.gif" style="height:150px; width:100px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 100px;">
+					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 50px;">
 						ONE PIECE
 					</td><hr color="white" width="990px">
 				</tr>
@@ -54,9 +54,9 @@
 			<table border="0">
 				<tr>
 					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+						<img src="<?php echo base_url()?>/img/adventure8.jpg" style="height:150px; width:100px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 100px;">
+					<td style="font-family: Tarijan Pro; font-size: 30px; color: white; padding-top: 50px;">
 						CRAYON SHINCHAN
 					</td><hr color="white" width="990px">
 				</tr>
@@ -73,9 +73,9 @@
 			<table border="0">
 				<tr>
 					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+						<img src="<?php echo base_url()?>/img/adventure9.jpg" style="height:150px; width:100px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 100px;">
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 50px;">
 						TINTIN AND THE PICAROS
 					</td><hr color="white" width="990px">
 				</tr>
@@ -92,9 +92,9 @@
 			<table border="0">
 				<tr>
 					<td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-top: 30px; margin-left: 20px; width: 160px; height: 220px;">
+						<img src="<?php echo base_url()?>/img/1.jpg" style="height:150px; width:100px; margin-left: 20px; width: 160px; height: 220px;">
 					</td><td rowspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 100px;">
+					<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 50px;">
 						TERJEBAK NOSTALGIA
 					</td><hr color="white" width="990px">
 				</tr>
