53d466ca3ce8b170083460bc83d4a4dc6158ab70
Nadia Siti Namira <nadiasitinamira@gmail.com>
2019-01-02 14:29:56 +0700

    Update about.php

--- application/views/userview/about.php
+++ application/views/userview/about.php
@@ -1,6 +1,16 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 250px;">
+		<table>
+			<tr>
+		<td style="font-family: Tarijan Pro; font-size: 25px;color: white;">
+			ABOUT THIS WEBSITE
+		</td>
+			</tr>
+		<td style="font-family: Tarijan Pro; font-size: 30px; margin-left: 20px; color: white; padding-top: 50px;">
+			ENJOY READ THE COMICS! YOU CAN CONTACT US BELOW HERE...
+		</td>
+		</table>
 		</div>
 	</div>
 </div>
