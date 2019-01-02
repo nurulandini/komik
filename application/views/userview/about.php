a76a12bb01031b34063313290a2477ea592fa05d
ayakwae <39100835+ayakwae@users.noreply.github.com>
2019-01-02 22:38:31 +0700

    Update about.php

--- application/views/userview/about.php
+++ application/views/userview/about.php
@@ -1,17 +1,20 @@
 <div class="row">
 	<div class="col-md-12">
-		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 250px;">
+		<div class="container" style="background-color: #8a9dab;border-width: 0px; width: 1120px; height: 500px;">
 	
-		 <u style="color:white;"><p style="font-family: Lucida Handwriting; font-size: 35px;color: white; padding-top: 30px; padding-bottom: 30px; text-align: center;">ABOUT US</p></u>
+		 <p style="font-family: Lucida Handwriting; font-size: 35px;color: white; padding-top: 20px; text-align: center; ">ABOUT US</p><hr color="white" >
 
 		<table>
-		<td style="font-family: Tarijan Pro; font-size: 27px;color: white; padding-bottom: 50px;">
+		<td style="font-family: Tarijan Pro; font-size: 23px;color: white; padding-bottom: 20px;">
 			ABOUT THIS WEBSITE
 		</td>
-		<td style="font-family: Tarijan Pro; font-size: 25px; color: white; padding-top: 50px;">
-			ENJOY READ THE COMICS! YOU CAN CONTACT US BELOW HERE...
-		</td>
 		</table>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white; margin-left: 75px;">ENJOY READ THE COMICS! YOU CAN CONTACT US BELOW HERE...</p>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white; padding-top: 50px; margin-left: 400px;">CONTACT</p>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white; padding-top: 10px;  margin-left: 400px;">EMAIL : komikQ@gmail.com</p>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white;  margin-left: 400px;">WEBSITE : komikQ.com</p>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white;  margin-left: 400px;">FACEBOOK : komikQ</p>
+		<p style="font-family: Tarijan Pro; font-size: 23px; color: white;  margin-left: 400px;">TWITTER : @komikQ</p>
 		</div>
 	</div>
 </div>
