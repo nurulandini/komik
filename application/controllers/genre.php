c1c6071c4db88dde3bef77d18144c4951e23cf6f
Nadia Siti Namira <nadiasitinamira@gmail.com>
2019-01-03 22:19:18 +0700

    controller genre

--- application/controllers/genre.php
+++ application/controllers/genre.php
@@ -10,6 +10,48 @@ class Genre extends CI_Controller
 		$data['page']='userview/genre';
 		$this->load->view('layout/main', $data);
 	}
+	
+	public function action()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/action';
+		$this->load->view('layout/main', $data);
+	}
+
+	public function adventure()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/adventure';
+		$this->load->view('layout/main', $data);
+	}
+	
+	public function comedy()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/comedy';
+		$this->load->view('layout/main', $data);
+	}
+
+	public function fantasy()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/fantasy';
+		$this->load->view('layout/main', $data);
+	}
+
+	public function horror()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/horror';
+		$this->load->view('layout/main', $data);
+	}
+
+	public function romance()
+	{
+		$data['border']='transparent';
+		$data['page']='userview/romance';
+		$this->load->view('layout/main', $data);
+	}
 
 }
 
