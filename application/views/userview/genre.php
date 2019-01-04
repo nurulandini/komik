60cca7b5adb37587cd22cffcced4748f672f0c5b
Nurul Andini <39187373+nurulandini@users.noreply.github.com>
2019-01-04 13:50:18 +0700

    nih

--- application/views/userview/genre.php
+++ application/views/userview/genre.php
@@ -4,23 +4,59 @@
 			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/action" style="color: white;">ACTION</a>
 		</div>
 		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-			<table>
-				<tr>
-					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action.jpg" style="width:160px; height: 220px;"></td><td>&nbsp;</td>
-					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action2.jpeg" style="width: 160px; height: 220px;"></td>
-				</tr>
-			</table>
+			<?php include 'sparqladventure1.php'?>
+			<?php $i=0;?>
+		 	<?php
+		 		$requestURL = getUrlDbpediaAdventure1($term);
+    			$responseArray =json_decode(request($requestURL),true); 
+		 	 foreach($responseArray["results"]["bindings"] as $key):?>
+		 		<?php if($i==0):?>
+		 				<?php endif ?>
+		 				<div class="col-md-3">
+		 					<div class="row">
+		 						<div class="col-md-12">
+		 							<div class="container-fluid" style="height: 150px">
+		 								<center>
+		 								<a href="<?php echo base_url()?>home/deskripsi/<?php echo $key['Id']['value']?>"><img src="<?php echo $key['Gambar']['value']?>" style="max-height: 150px;max-width: 150px"></a>
+		 								</center>
+		 							</div>
+		 						</div>
+		 					</div>
+		 				</div>
+		 				<?php $i++;?>
+		 				<?php if($i==4):?>
+		 				<?php $i=0;?>
+		 				<?php endif?>
+		 		<?php endforeach?>
 		</div>
 		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
 			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/adventure" style="color: white;">ADVENTURE</a>
 		</div>
 		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-			<table>
-				<tr>
-					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/naruto.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/2.jpg" style="width: 160px; height: 220px;"></td>
-				</tr>
-			</table>
+			<?php include 'sparqladventure1.php'?>
+			<?php $i=0;?>
+		 	<?php
+		 		$requestURL = getUrlDbpediaAdventure1($term);
+    			$responseArray =json_decode(request($requestURL),true); 
+		 	 foreach($responseArray["results"]["bindings"] as $key):?>
+		 		<?php if($i==0):?>
+		 				<?php endif ?>
+		 				<div class="col-md-3">
+		 					<div class="row">
+		 						<div class="col-md-12">
+		 							<div class="container-fluid" style="height: 150px">
+		 								<center>
+		 								<a href="<?php echo base_url()?>home/deskripsi/<?php echo $key['Id']['value']?>"><img src="<?php echo $key['Gambar']['value']?>" style="max-height: 150px;max-width: 150px"></a>
+		 								</center>
+		 							</div>
+		 						</div>
+		 					</div>
+		 				</div>
+		 				<?php $i++;?>
+		 				<?php if($i==4):?>
+		 				<?php $i=0;?>
+		 				<?php endif?>
+		 		<?php endforeach?>
 		</div>
 	</div>
 	<div class="col-md-4">
---
+++ application/views/userview/sparqlcomedy1.php
@@ -0,0 +1,41 @@
+1<?php
+
+function getUrlDbpediaComedy($term)
+{
+	$format = 'json';
+	$query =
+	"
+	PREFIX dbr: <http://dbpedia.org/resource/>
+	PREFIX dbo: <http://dbpedia.org/ontology/>
+	PREFIX dbpedia2: <http://dbpedia.org/property/>
+
+
+	SELECT ?Id ?Gambar ?Title ?Genre ?Author ?Synopsis WHERE {
+	?Judul a dbo:Manga .
+	?Judul dbo:wikiPageID ?Id .
+	?Judul dbo:thumbnail ?Gambar .
+	?Judul rdfs:label ?Title .
+	?Judul dbpedia2:genre ?Aliran .
+	?Aliran rdfs:label ?Genre .
+	?Judul dbo:author ?penulis .
+	?penulis rdfs:label ?Author .
+	?Judul dbo:abstract ?Synopsis .
+	FILTER (lang(?Title)='en') .
+	FILTER (lang(?Genre)='en') .
+	FILTER (lang(?Author)='en') .
+	FILTER (lang(?Synopsis)='en') .
+	FILTER regex(?Genre,'^Comedy')
+	}";
+
+	$searchUrl = 'http://dbpedia.org/sparql?'
+	.'query='.urlencode($query)
+	.'&format='.$format;
+
+	return $searchUrl;
+}
+
+include 'arraybuatan.php';
+
+$term='';
+
+?>
