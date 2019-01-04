d09da0714604fa92678f7f1d1bf161a1d3dbdee6
Nadia Siti Namira <nadiasitinamira@gmail.com>
2019-01-04 10:07:01 +0700

    udh gk berjarak ayakwae

--- application/views/userview/genre.php
+++ application/views/userview/genre.php
@@ -47,7 +47,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="row" style="padding-top: 2em;padding-bottom: -100px">
+		<div class="row" style="padding-top: 2em;">
 			<div class="col-md-4">
 				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
 					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/fantasy" style="color: white;">FANTASY</a>
@@ -64,7 +64,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="row" style="padding-top: -100px;">
+		<div class="row" style="margin-top: -10px;">
 			<div class="col-md-4">
 				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
 					<table>
