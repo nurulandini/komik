3db994acf4014523732084727500090902360430
Nadia Siti Namira <nadiasitinamira@gmail.com>
2019-01-04 10:37:23 +0700

    Update genre.php

--- application/views/userview/genre.php
+++ application/views/userview/genre.php
@@ -1,97 +1,70 @@
 <div class="row" style="padding-top: 2em">
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/action" style="color: white;">ACTION</a>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/adventure" style="color: white;">ADVENTURE</a>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px; font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/comedy" style="color: white;">COMEDY</a>
-				</div>
-			</div>
+	<div class="col-md-4">
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/action" style="color: white;">ACTION</a>
 		</div>
-		<div class="row" style="margin-top: -10px;">
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action2.jpeg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/naruto.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/2.jpg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/juki.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/tahi.jpg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action.jpg" style="width:160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/action2.jpeg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
 		</div>
-		<div class="row" style="padding-top: 2em;">
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/fantasy" style="color: white;">FANTASY</a>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/horror" style="color: white;">HORROR</a>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px; font-family: Trajan Pro; font-size: 30px;width: 360px;">
-					<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/romance" style="color: white;">ROMANCE</a>
-				</div>
-			</div>
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/adventure" style="color: white;">ADVENTURE</a>
 		</div>
-		<div class="row" style="margin-top: -10px;">
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy.png" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy2.jpg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror2.jpeg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror3.jpg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
-					<table>
-						<tr>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/1.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
-							<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/romance2.jpg" style="width: 160px; height: 220px;"></td>
-						</tr>
-					</table>
-				</div>
-			</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/naruto.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/2.jpg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px; font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/comedy" style="color: white;">COMEDY</a>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/juki.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/tahi.jpg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/fantasy" style="color: white;">FANTASY</a>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy.png" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/fantasy2.jpg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px;font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/horror" style="color: white;">HORROR</a>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror2.jpeg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/horror3.jpg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #0e2854;height: 60px;color: white;text-align: center;padding-top: 3px; font-family: Trajan Pro; font-size: 30px;width: 360px;">
+			<a  class="nav-link text-uppercase text-expanded" href="<?php echo base_url()?>Genre/romance" style="color: white;">ROMANCE</a>
+		</div>
+		<div class="container" style="border-width: 0px;background-color: #8a9dab;height: 300px;width: 360px">
+			<table>
+				<tr>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/1.jpg" style="width: 160px; height: 220px;"></td><td>&nbsp;</td>
+					<td width="300px" height="300px"><img src="<?php echo base_url()?>/img/romance2.jpg" style="width: 160px; height: 220px;"></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
